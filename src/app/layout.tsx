938e306639efcdfc4f7b05355b950acb906b2e29
Tornike <tornikekapan@gmail.com>
2024-06-23 13:10:13 +0300

    master: add meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import { Theme } from '@radix-ui/themes'
 import { ACCENT_COLOR, DEFAULT_APPEARANCE, GRAY_COLOR } from '~/constants/theme'
+import { SITE_DESCRIPTION, SITE_NAME, SITE_URL } from '~/constants/metaData'
 import type { Metadata } from 'next'
 import '@radix-ui/themes/styles.css'
 import './styles/globals.css'
@@ -9,9 +10,34 @@ import './styles/reset.css'
 const inter = Inter({ subsets: ['latin'], display: 'swap' })
 
 export const metadata: Metadata = {
-  title: 'Watchlist',
-  description:
-    'Who needs a social life when there are so many things to watch?',
+  metadataBase: new URL(SITE_URL),
+  title: SITE_NAME,
+  description: SITE_DESCRIPTION,
+  applicationName: SITE_NAME,
+  authors: [
+    { name: 'torkinos', url: 'https://torkinos.com' },
+    { name: 'KaanDzai', url: 'https://kaandzai.webflow.io' },
+  ],
+  keywords: ['movies', 'tv shows', 'watchlist', 'discover'],
+  viewport: 'width=device-width, initial-scale=1',
+  creator: 'Torkinos',
+  publisher: 'Torkinos',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    siteName: SITE_NAME,
+    title: SITE_NAME,
+    description: SITE_DESCRIPTION,
+    url: SITE_URL,
+    images: [
+      {
+        url: 'https://watchlist.torkinos.com/images/og-image.png',
+        alt: 'Watchlist',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
---
+++ src/constants/metaData.ts
@@ -0,0 +1,4 @@
+export const SITE_NAME = 'Watchlist'
+export const SITE_URL = 'https://watchlist.torkinos.com'
+export const SITE_DESCRIPTION =
+  'Who needs a social life when there are so many things to watch?'
